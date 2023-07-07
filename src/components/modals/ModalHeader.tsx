4bfa700a68cbfee4d98e5746a8f61eefdb7dd4d7
seoyoonyi <yiseoyoon@gmail.com>
2023-07-07 18:19:01 +0900

    Chore: ProgressBar 컴포넌트에서 불필요한 주석 제거

--- src/components/modals/ModalHeader.tsx
+++ src/components/modals/ModalHeader.tsx
@@ -11,7 +11,6 @@ const ModalHeader = ({ title, onClose }: IModalHeaderProps) => {
 		<div className="flex justify-between bg-retroBlue  py-[3px] px-[6px] text-white">
 			<h2 className="font-bold font-eng">{title}</h2>
 			<div className="">
-				{/* w-[21px] h-[21px] flex justify-center items-center */}
 				<Button
 					onClick={onClose}
 					className=" w-[21px] h-[21px] flex justify-center items-center"
