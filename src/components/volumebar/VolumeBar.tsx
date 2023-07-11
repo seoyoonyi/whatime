1ddf4932ce7bb5a12977503c8024a24e48eb16f6
seoyoonyi <yiseoyoon@gmail.com>
2023-07-12 00:39:59 +0900

    Rename: IPlayer 경로 변경

--- src/components/volumebar/VolumeBar.tsx
+++ src/components/volumebar/VolumeBar.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import styles from './volumebar.module.css';
-import { IPlayer } from '../modals/MusicModal';
+import { IPlayer } from '../types/types';
 
 interface IVolumeBarProps {
 	player: IPlayer | null;
