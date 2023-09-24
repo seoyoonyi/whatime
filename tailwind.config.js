ca92b1432c3792df091d79b6b34bb3374940998f
seoyoonyi <yiseoyoon@gmail.com>
2023-09-25 01:57:58 +0900

    Chore: tailwindcss.config 업데이트, postcss, autoprefixer 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-	mode: 'jit',
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		colors: {
