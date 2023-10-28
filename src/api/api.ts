ba6a5121f8ad6bebb745f5f06a3cda92956c15c6
seoyoonyi <yiseoyoon@gmail.com>
2023-10-28 22:00:03 +0900

    Fix: API 통신 시 데이터 없을 때, 목데이터로 로직 수정

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,12 +4,16 @@ import mockData from '../data/mock.json';
 const apiUrl = process.env.REACT_APP_API_URL as string;
 
 export const fetchSongs = async () => {
-	if (process.env.NODE_ENV === 'development') {
+	try {
+		const response = await axios.get(apiUrl);
+		if (response.data && Array.isArray(response.data.data) && response.data.data.length > 0) {
+			return response.data.data;
+		} else {
+			console.warn('No data or invalid response format from API, using mock data instead');
+			return mockData.data;
+		}
+	} catch (error) {
+		console.error('Failed to fetch songs from API, using mock data instead:', error);
 		return mockData.data;
 	}
-	const response = await axios.get(apiUrl);
-	if (response.data && Array.isArray(response.data.data)) {
-		return response.data.data;
-	}
-	throw new Error('Invalid response format.');
 };
