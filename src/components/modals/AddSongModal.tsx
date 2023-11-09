363748c6c7ca387f517ca1f89071252dbd77603a
seoyoonyi <yiseoyoon@gmail.com>
2023-11-09 23:00:26 +0900

    Fix: 곡 추가 후 확인 메시지 및 모달 초기화 UX 개선

--- src/components/modals/AddSongModal.tsx
+++ src/components/modals/AddSongModal.tsx
@@ -9,32 +9,48 @@ interface IAddSongModal {
 
 const AddSongModal = ({ isOpen, onClose }: IAddSongModal) => {
 	const AddSongModalRef = useRef(null);
-
 	const [showConfirm, setShowConfirm] = useState(false);
 
 	const handleAddToPlaylist = () => {
-		console.log('Added to Playlist');
+		setShowConfirm(true);
 	};
 
 	const handleAddToMixtape = () => {
 		console.log('Added to mixtape');
 	};
+
+	const handleClose = () => {
+		setShowConfirm(false);
+		onClose();
+	};
+
 	if (!isOpen) return null;
 	return (
 		<Modal
 			className="absolute"
 			open={isOpen}
-			onClose={onClose}
+			onClose={handleClose}
 			modalRef={AddSongModalRef}
 			style={{ zIndex: 100 }}
 		>
-			<div className="flex flex-col px-20 py-10 space-y-1">
-				<Button className="w-[192px] py-[2px]" onClick={handleAddToPlaylist}>
-					Add to playlist
-				</Button>
-				<Button className="w-[192px] py-[2px]" onClick={handleAddToMixtape}>
-					Add to mixtape
-				</Button>
+			<div className="flex flex-col justify-center px-20 py-10 space-y-1">
+				{showConfirm ? (
+					<>
+						<h4 className="pb-2">Added 1 song to the playlist.</h4>
+						<Button className="w-[192px] py-[2px]" onClick={handleAddToMixtape}>
+							Confirm
+						</Button>
+					</>
+				) : (
+					<>
+						<Button className="w-[192px] py-[2px]" onClick={handleAddToPlaylist}>
+							Add to playlist
+						</Button>
+						<Button className="w-[192px] py-[2px]" onClick={handleAddToMixtape}>
+							Add to mixtape
+						</Button>
+					</>
+				)}
 			</div>
 		</Modal>
 	);
