e69f294b50fc89fa7966ce2f7ff857c8a051cea3
seoyoonyi <yiseoyoon@gmail.com>
2024-03-16 11:36:43 +0900

    Feat: react-query-devtools 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@react95/icons/icons.css';
 import './App.css';
 import MainPage from './page/MainPage';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<MainPage></MainPage>
+			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	);
 }
