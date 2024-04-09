6d1fb7657cabaf7ec6aa7dc2330cd7e2e16546af
seoyoonyi <yiseoyoon@gmail.com>
2024-04-09 14:43:41 +0900

    Refactor: 명시적으로 이름을 지정하여 zustand 내보내기 수정

--- src/stores/useModalStore.tsx
+++ src/stores/useModalStore.tsx
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import { ModalType } from '../types/modalTypes';
 
 type ModalStateType = Record<ModalType, { isOpen: boolean; zIndex: number; isMinimized: boolean }>;
--- src/stores/useMusicStore.tsx
+++ src/stores/useMusicStore.tsx
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import { ISong } from '../types/types';
 
 interface IMusicState {
